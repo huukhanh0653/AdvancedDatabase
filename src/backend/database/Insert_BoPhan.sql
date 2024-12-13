c3463aea5cb728aeebfc6365ca68be239d03ec12
huukhanh0653 <huukhanh0653@gmail.om>
2024-12-13 13:18:04 +0700

    generated database

--- src/backend/database/Insert_BoPhan.sql
+++ src/backend/database/Insert_BoPhan.sql
@@ -1,9 +1,10 @@
-
 DELETE FROM BoPhan 
 GO 
 
-INSERT INTO BoPhan (MaBP, TenBoPhan, Luong) VALUES (1, N'Quản lý', 20000000);
+SET IDENTITY_INSERT BoPhan ON 
+ INSERT INTO BoPhan (MaBP, TenBoPhan, Luong) VALUES (1, N'Quản lý', 20000000);
 INSERT INTO BoPhan (MaBP, TenBoPhan, Luong) VALUES (2, N'Phục vụ', 10000000);
 INSERT INTO BoPhan (MaBP, TenBoPhan, Luong) VALUES (3, N'Lễ tân', 8000000);
 INSERT INTO BoPhan (MaBP, TenBoPhan, Luong) VALUES (4, N'Bếp', 15000000);
-INSERT INTO BoPhan (MaBP, TenBoPhan, Luong) VALUES (5, N'Thu ngân', 12000000); 
+INSERT INTO BoPhan (MaBP, TenBoPhan, Luong) VALUES (5, N'Thu ngân', 12000000);
+INSERT INTO BoPhan (MaBP, TenBoPhan, Luong) VALUES (6, N'Quản trị viên', 25000000); 
