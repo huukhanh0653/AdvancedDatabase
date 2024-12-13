d684d3ee59c2f81875d06c0a2244dfea41e5c2a8
huukhanh0653 <huukhanh0653@gmail.om>
2024-12-13 22:52:37 +0700

    insert PhieuDanhGia

--- src/backend/routes/admin.js
+++ src/backend/routes/admin.js
@@ -3,7 +3,7 @@ const passport = require("../middleware/passport");
 const { isEmployee } = require("../middleware/auth");
 var router = express.Router();
 const { sql, poolPromise } = require("../model/dbConfig");
-const { executeProcedure } = require("../middleware/queryDB");
+const { executeProcedure, query_paginating } = require("../middleware/queryDB");
 require("../model/query");
 
 router.get("/get-all-bophan", async function (req, res, next) {
@@ -18,22 +18,44 @@ router.get("/get-all-bophan", async function (req, res, next) {
 });
 
 router.get("/get-all-nhanvien", async function (req, res, next) {
-  console.log(req.query);
+  console.log(req.query)
 
-  // let query = `SELECT * FROM NHANVIEN
-  // ORDER BY MaNV
-  // OFFSET ${req.query.PageSize * (req.query.PageNumber - 1)} ROWS
-  // FETCH NEXT ${req.query.PageSize} ROWS ONLY`;
+  let pageSize = req.query.PageSize ? parseInt(req.query.PageSize, 10) : 25;
+  let pageNumber = req.query.PageNumber ? parseInt(req.query.PageNumber, 10) : 1;
 
-  let query = `SELECT * FROM NHANVIEN`;
-
-  const pool = await poolPromise;
-  const result = await pool.request().query(query);
+  const result = await query_paginating("NHANVIEN", "MaNV", pageSize, pageNumber);
   if (!result) {
     return res.status(500).json({ message: "Internal server error" });
   }
-  console.log(result.recordset);
   return res.status(200).json(result.recordset);
 });
+
+router.get("/get-all-khachhang", async function (req, res, next) {
+  console.log(req.query)
+
+  let pageSize = req.query.PageSize ? parseInt(req.query.PageSize, 10) : 25;
+  let pageNumber = req.query.PageNumber ? parseInt(req.query.PageNumber, 10) : 1;
+
+  const result = await query_paginating("KHACHHANG", "MaKH", pageSize, pageNumber);
+  if (!result) {
+    return res.status(500).json({ message: "Internal server error" });
+  }
+  return res.status(200).json(result.recordset);
+});
+
+router.get("/get-all-hoadon", async function (req, res, next) {
+  console.log(req.query)
+
+  let pageSize = req.query.PageSize ? parseInt(req.query.PageSize, 10) : 25;
+  let pageNumber = req.query.PageNumber ? parseInt(req.query.PageNumber, 10) : 1;
+
+  const result = await query_paginating("HOADON", "MaHD", pageSize, pageNumber);
+  if (!result) {
+    return res.status(500).json({ message: "Internal server error" });
+  }
+  return res.status(200).json(result.recordset);
+});
+
+
 
 module.exports = router;
